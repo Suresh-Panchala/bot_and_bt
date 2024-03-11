7d87c135e03000defc25d8c6bffb2a25e511872e
Suresh Panchala <sureshpanchala@hotmail.com>
2024-03-12 02:55:27 +0530

    Add files via upload

---
+++ bot.ino
@@ -0,0 +1,38 @@
+byte m1 = 13;
+byte m2 = 12;
+byte m3 = 14;
+byte m4 = 27;
+void setup() {
+  pinMode(m1,OUTPUT);
+  pinMode(m2,OUTPUT);
+  pinMode(m3,OUTPUT);
+  pinMode(m4,OUTPUT);
+}
+
+void loop() {
+  left();
+}
+void forward(){
+  digitalWrite(m1,LOW);
+  digitalWrite(m2,HIGH);
+  digitalWrite(m3,LOW);
+  digitalWrite(m4,HIGH);
+}
+void reverse(){
+  digitalWrite(m1,HIGH);
+  digitalWrite(m2,LOW);
+  digitalWrite(m3,HIGH);
+  digitalWrite(m4,LOW);
+}
+void right(){
+  digitalWrite(m1,LOW);
+  digitalWrite(m2,HIGH);
+  digitalWrite(m3,HIGH);
+  digitalWrite(m4,LOW);
+}
+void left(){
+  digitalWrite(m1,HIGH);
+  digitalWrite(m2,LOW);
+  digitalWrite(m3,LOW);
+  digitalWrite(m4,HIGH);
+}
